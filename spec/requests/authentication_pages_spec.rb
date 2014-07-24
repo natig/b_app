0afb639f80d0205a8abae4e4f11dbe3f1896979f
natig <natig1906@ya.ru>
2014-07-24 18:49:45 +0400

    add sign-up-in-out + testing

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "Аутентификации" do  
+
+  subject { page }
+  
+  describe "Страница аутентификации" do
+    before { visit signin_path}
+    
+    it { should have_title('Аутентификация') }
+    it { should have_content('Вход') }
+    
+    describe "невалидная аутентификация" do      
+      before { click_button "Вход" }
+      
+      it { should have_title('Аутентификация') }
+      it { should have_selector('div.alert.alert-error') }      
+    end
+    
+    describe "валидная информация" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Электронный адрес", with: user.email.upcase
+	     fill_in "Пароль",            with: user.password
+	     click_button "Вход"
+      end   
+        it { should have_link('Главная') }
+        it { should have_title(user.name) }
+
+      describe "теперь выход" do
+        before do
+          visit root_path
+          click_link "Закрыть сессию"
+        end
+        it { should have_link('Зайти под именем') }
+      end    
+    end
+  end
+end
