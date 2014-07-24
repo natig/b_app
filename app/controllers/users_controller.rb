eb3617a6dfa11280155892357f6ad795fee05711
natig <natig1906@ya.ru>
2014-07-24 11:40:21 +0400

    signup add changes user_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 	 if @user.save
+	   flash[:success] = "Добро пожаловать в приложение \"B\"!"
 	   redirect_to @user
 	 else
 	   render 'new'
